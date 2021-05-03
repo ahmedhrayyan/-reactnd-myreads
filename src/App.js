88d1bd3f6eca1405201a00fcfd598d51132144af
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-05-03 13:40:00 +0200

    Use react-router-dom to handle routes (pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 // import * as BooksAPI from './BooksAPI'
 import Home from "./Home";
 import "./App.css";
+import { Route, Switch } from "react-router";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="app">
-      <Home />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route>
+          not found!
+        </Route>
+      </Switch>
     </div>
   );
 }
