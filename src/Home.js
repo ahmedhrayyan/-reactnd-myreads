87e0c0e9951e4bed89b27735dc3730c6ef3c697f
Ahmed Hamed <ahmedhrayyan@outlook.com>
2021-05-03 13:45:14 +0200

    Use react router links to handle navigation

--- src/Home.js
+++ src/Home.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Home() {
   return (
     <div className="list-books">
@@ -244,9 +246,7 @@ function Home() {
         </div>
       </div>
       <div className="open-search">
-        <button onClick={() => this.setState({ showSearchPage: true })}>
-          Add a book
-        </button>
+        <Link to="/search">Add a book</Link>
       </div>
     </div>
   );
